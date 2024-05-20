447394d61135294517e3fa71c1205bd965d7a2ce
Mladjan Gadzic <gadzic.mladjan@gmail.com>
2024-05-20 16:40:45 +0200

    Fix format

--- src/test/java/com/example/instagram/InstagramApplicationTests.java
+++ src/test/java/com/example/instagram/InstagramApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class InstagramApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
