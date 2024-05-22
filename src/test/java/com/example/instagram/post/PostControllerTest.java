26e161a9e754cdc02179a966b247157186dc2cd7
Katarina Savic <kksavic97@gmil.com>
2024-05-22 13:45:35 +0200

    Integration tests unified and postMapping rearranged

--- src/test/java/com/example/instagram/post/PostControllerTest.java
+++ src/test/java/com/example/instagram/post/PostControllerTest.java
@@ -1,15 +1,9 @@
 package com.example.instagram.post;
 
-import com.example.instagram.dto.PostDto;
-import com.example.instagram.entity.Post;
-import com.example.instagram.mapper.PostMapper;
-import com.example.instagram.service.PostService;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
