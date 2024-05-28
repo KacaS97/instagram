a73a6806e5ced45d0927d4a173b2898fd03a6042
Katarina Savic <kksavic97@gmil.com>
2024-05-28 22:27:56 +0200

    Fix repository

--- src/main/java/com/example/instagram/repository/PostRepository.java
+++ src/main/java/com/example/instagram/repository/PostRepository.java
@@ -2,9 +2,6 @@ package com.example.instagram.repository;
 
 import com.example.instagram.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-
 }
