1c06490f2ed6d98c0868abd080f8cee1b7310af5
Katarina Savic <kksavic97@gmil.com>
2024-06-03 23:06:48 +0200

    Fix conflicts

--- src/test/java/com/example/instagram/service/ImageServiceTest.java
+++ src/test/java/com/example/instagram/service/ImageServiceTest.java
@@ -7,13 +7,11 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import com.example.instagram.entity.Image;
-import com.example.instagram.repository.ImageRepository;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
@@ -21,14 +19,8 @@ import org.springframework.mock.web.MockMultipartFile;
 @ExtendWith(MockitoExtension.class)
 public class ImageServiceTest {
 
-  @Mock
-  private ImageRepository imageRepository;
-
   @InjectMocks
   private ImageService imageService;
-
-  @Mock
-  private ImageRepository imageRepository;
 
   @Test
   public void givenBuildImage_whenNoExceptionOccurs_thenImageIsReturned() {
